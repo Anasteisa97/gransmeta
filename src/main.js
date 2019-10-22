6a6ba8cdc2d79934387746d82974e28b1ec84461
Nastya <nas.lazarewa@yandex.ru>
2019-10-22 17:04:06 +0300

    22 октября how-choose

--- src/main.js
+++ src/main.js
@@ -13,3 +13,12 @@ jQuery(function($) {
 });
 
 
+$('.how-choose__item-box').on('click', function (e) {
+  e.preventDefault;
+  //$(this).toggleClass('how-choose__item-box_after');
+  $('.how-choose__item-box_after').toggleClass('how-choose__item-box_after_click');
+  $('.how-choose__item-box:before').css('background','#f8484f');
+
+});
+
+
