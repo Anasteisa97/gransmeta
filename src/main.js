9262d71cc779d2b56c68c9ace69858d500a44115
DANILIKYS <ooo.kerm@yandex.ru>
2019-10-23 16:58:43 +0300

    Данил, советы.

--- src/main.js
+++ src/main.js
@@ -1,24 +1,23 @@
 
 
-jQuery(function($) {
+$(document).ready(function () {
 
-  $(document).ready(function () {
+  console.log('YEP');
 
-    console.log('YEP');
+  $('.how-choose__item-box').on('click', function(e) {
+    event.preventDefault(e);
 
-    
+    $(this).closest('.how-choose__item').find('.how-choose__item-box_after').toggleClass('how-choose__item-box_after_click');
+
+    //$(this).toggleClass('how-choose__item-box_after');
+    // $('.how-choose__item-box_after').toggleClass('how-choose__item-box_after_click');
+    $('.how-choose__item-box:before').css('background','#f8484f');
 
   });
 
 });
 
 
-$('.how-choose__item-box').on('click', function (e) {
-  e.preventDefault;
-  //$(this).toggleClass('how-choose__item-box_after');
-  $('.how-choose__item-box_after').toggleClass('how-choose__item-box_after_click');
-  $('.how-choose__item-box:before').css('background','#f8484f');
 
-});
 
 
