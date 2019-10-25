6dcc8f0a00f29c7f454ecb17bfe5e31eb5dcda57
Anasteisa97 <nas.lazarewa@yandex.ru>
2019-10-25 17:00:03 +0300

    25 октября, почти всё (десктоп)

--- src/main.js
+++ src/main.js
@@ -9,8 +9,18 @@ $(document).ready(function () {
 
     //$(this).toggleClass('how-choose__item-box_after');
     // $('.how-choose__item-box_after').toggleClass('how-choose__item-box_after_click');
-    $('.how-choose__item-box:before').css('background','#f8484f');
+    //$('.how-choose__item-box:before').css('background','#f8484f');
 
+  });
+
+
+  $('.btn.btn-packages').on('click', function (e) {
+    event.preventDefault(e);
+    $('.form-call').fadeIn();
+  });
+
+  $('.def-form__back, .def-form__close').on('click', function () {
+    $('.form-call').fadeOut();
   });
 
 });
